6438e00c77c8f6291305a5ad8e18b9d1959a744c
George Wu <george@georgewu.ca>
2015-10-28 10:34:58 -0400

    Slight syntax error

--- app/components/Items.jsx
+++ app/components/Items.jsx
@@ -5,94 +5,95 @@ export default class Items extends React.Component {
 		return <div>
 
 				<div className="container">
-				<div className="row">
-					<div className="three columns" data-offset="-100">
-						<a href="#">
-							<div className="item overlay red" >
-								{/*<video  src="https://www.leia3d.com/wp-content/uploads/2015/09/leia3d-devkit-eye.mp4"  poster="https://www.leia3d.com/wp-content/uploads/2014/11/leia3d-devkit-eye.jpg" preload loop></video> */}
-								<img src="app/images/MaideControl.jpeg" className= "object-fit_cover"/>
-								<h5 >Maide Control</h5>
 
-							</div>
-						</a>
-					</div>
-					<div className="three columns" data-offset="-100">
-						<a href="#">
-							<div className="item overlay red" >
-								{/*<video  src="https://www.leia3d.com/wp-content/uploads/2015/09/leia3d-devkit-eye.mp4"  poster="https://www.leia3d.com/wp-content/uploads/2014/11/leia3d-devkit-eye.jpg" preload loop></video> */}
-								<img src="app/images/kano-crop.jpeg" className= "object-fit_cover"/>
-								<h5 >Kano App</h5>
-							</div>
-						</a>
-					</div>
-					<div className="three columns" data-offset="-100">
-						<a href="#">
-							<div className="item overlay red" >
-								{/*<video  src="https://www.leia3d.com/wp-content/uploads/2015/09/leia3d-devkit-eye.mp4"  poster="https://www.leia3d.com/wp-content/uploads/2014/11/leia3d-devkit-eye.jpg" preload loop></video> */}
-								<img src="app/images/Maven_Intro.png" className= "object-fit_cover"/>
-								<h5 >Maide Control</h5>
+					VIEW: <span className="subtitle"><a href="#">PROJECTS</a></span>
+					<p / >
+					
+					<div className="row">
+						<div className="three columns" data-offset="-100">
+							<a href="#">
+								<div className="item overlay red" >
+									{/*<video  src="https://www.leia3d.com/wp-content/uploads/2015/09/leia3d-devkit-eye.mp4"  poster="https://www.leia3d.com/wp-content/uploads/2014/11/leia3d-devkit-eye.jpg" preload loop></video> */}
+									<img src="app/images/MaideControl.jpeg" className= "object-fit_cover"/>
+									<h5 >Maide Control</h5>
 
-							</div>
-						</a>
-					</div>
-					<div className="three columns" data-offset="-100">
-						<a href="#">
-							<div className="item overlay red" >
-								{/*<video  src="https://www.leia3d.com/wp-content/uploads/2015/09/leia3d-devkit-eye.mp4"  poster="https://www.leia3d.com/wp-content/uploads/2014/11/leia3d-devkit-eye.jpg" preload loop></video> */}
-								<img src="app/images/Influitive_Dashboard.png" className= "object-fit_cover"/>
-								<h5 >Maide Control</h5>
+								</div>
+							</a>
+						</div>
+						<div className="three columns" data-offset="-100">
+							<a href="#">
+								<div className="item overlay red" >
+									{/*<video  src="https://www.leia3d.com/wp-content/uploads/2015/09/leia3d-devkit-eye.mp4"  poster="https://www.leia3d.com/wp-content/uploads/2014/11/leia3d-devkit-eye.jpg" preload loop></video> */}
+									<img src="app/images/kano-crop.jpeg" className= "object-fit_cover"/>
+									<h5 >Kano App</h5>
+								</div>
+							</a>
+						</div>
+						<div className="three columns" data-offset="-100">
+							<a href="#">
+								<div className="item overlay red" >
+									{/*<video  src="https://www.leia3d.com/wp-content/uploads/2015/09/leia3d-devkit-eye.mp4"  poster="https://www.leia3d.com/wp-content/uploads/2014/11/leia3d-devkit-eye.jpg" preload loop></video> */}
+									<img src="app/images/Maven_Intro.png" className= "object-fit_cover"/>
+									<h5 >Maide Control</h5>
 
-							</div>
-						</a>
+								</div>
+							</a>
+						</div>
+						<div className="three columns" data-offset="-100">
+							<a href="#">
+								<div className="item overlay red" >
+									{/*<video  src="https://www.leia3d.com/wp-content/uploads/2015/09/leia3d-devkit-eye.mp4"  poster="https://www.leia3d.com/wp-content/uploads/2014/11/leia3d-devkit-eye.jpg" preload loop></video> */}
+									<img src="app/images/Influitive_Dashboard.png" className= "object-fit_cover"/>
+									<h5 >Maide Control</h5>
+
+								</div>
+							</a>
+						</div>
 					</div>
-				</div>
 
 
-				<div className="row">
-					<div className="three columns" data-offset="-100">
-						<a href="#">
-							<div className="item overlay red" >
-								{/*<video  src="https://www.leia3d.com/wp-content/uploads/2015/09/leia3d-devkit-eye.mp4"  poster="https://www.leia3d.com/wp-content/uploads/2014/11/leia3d-devkit-eye.jpg" preload loop></video> */}
-								<img src="app/images/Umbra_Swivel.png" className= "object-fit_cover"/>
-								<h5 >Maven App</h5>
+					<div className="row">
+						<div className="three columns" data-offset="-100">
+							<a href="#">
+								<div className="item overlay red" >
+									{/*<video  src="https://www.leia3d.com/wp-content/uploads/2015/09/leia3d-devkit-eye.mp4"  poster="https://www.leia3d.com/wp-content/uploads/2014/11/leia3d-devkit-eye.jpg" preload loop></video> */}
+									<img src="app/images/Umbra_Swivel.png" className= "object-fit_cover"/>
+									<h5 >Maven App</h5>
 
-							</div>
-						</a>
-					</div>
-					<div className="three columns" data-offset="-100">
-						<a href="#">
-							<div className="item overlay red" >
-								{/*<video  src="https://www.leia3d.com/wp-content/uploads/2015/09/leia3d-devkit-eye.mp4"  poster="https://www.leia3d.com/wp-content/uploads/2014/11/leia3d-devkit-eye.jpg" preload loop></video> */}
-								<img src="app/images/Influitive_Dashboard.png" className= "object-fit_cover"/>
-								<h5 >Influitive Dashboard</h5>
-							</div>
-						</a>
-					</div>
-					<div className="three columns" data-offset="-100">
-						<a href="#">
-							<div className="item overlay red" >
-								{/*<video  src="https://www.leia3d.com/wp-content/uploads/2015/09/leia3d-devkit-eye.mp4"  poster="https://www.leia3d.com/wp-content/uploads/2014/11/leia3d-devkit-eye.jpg" preload loop></video> */}
-								<img src="app/images/MaideControl.jpeg" className= "object-fit_cover"/>
-								<h5 >Maide Control</h5>
+								</div>
+							</a>
+						</div>
+						<div className="three columns" data-offset="-100">
+							<a href="#">
+								<div className="item overlay red" >
+									{/*<video  src="https://www.leia3d.com/wp-content/uploads/2015/09/leia3d-devkit-eye.mp4"  poster="https://www.leia3d.com/wp-content/uploads/2014/11/leia3d-devkit-eye.jpg" preload loop></video> */}
+									<img src="app/images/Influitive_Dashboard.png" className= "object-fit_cover"/>
+									<h5 >Influitive Dashboard</h5>
+								</div>
+							</a>
+						</div>
+						<div className="three columns" data-offset="-100">
+							<a href="#">
+								<div className="item overlay red" >
+									{/*<video  src="https://www.leia3d.com/wp-content/uploads/2015/09/leia3d-devkit-eye.mp4"  poster="https://www.leia3d.com/wp-content/uploads/2014/11/leia3d-devkit-eye.jpg" preload loop></video> */}
+									<img src="app/images/MaideControl.jpeg" className= "object-fit_cover"/>
+									<h5 >Maide Control</h5>
 
-							</div>
-						</a>
+								</div>
+							</a>
+						</div>
+						<div className="three columns" data-offset="-100">
+							<a href="#">
+								<div className="item overlay red" >
+									{/*<video  src="https://www.leia3d.com/wp-content/uploads/2015/09/leia3d-devkit-eye.mp4"  poster="https://www.leia3d.com/wp-content/uploads/2014/11/leia3d-devkit-eye.jpg" preload loop></video> */}
+									<img src="app/images/kano-crop.jpeg" className= "object-fit_cover"/>
+									<h5 >Kano App</h5>
+								</div>
+							</a>
+						</div>
 					</div>
-					<div className="three columns" data-offset="-100">
-						<a href="#">
-							<div className="item overlay red" >
-								{/*<video  src="https://www.leia3d.com/wp-content/uploads/2015/09/leia3d-devkit-eye.mp4"  poster="https://www.leia3d.com/wp-content/uploads/2014/11/leia3d-devkit-eye.jpg" preload loop></video> */}
-								<img src="app/images/kano-crop.jpeg" className= "object-fit_cover"/>
-								<h5 >Kano App</h5>
-							</div>
-						</a>
-					</div>
-				</div>
-					<p / >				
-					<h6>
-						<b>01</b>.
-						 <span className="subtitle">PROJECTS</span>
-					</h6>
+				
+				
 				</div>
 		</div>;
 	}
